f503e808e6f59af2a5d5310193d883855192fb6f
liamappenidno <appendinoliam@gmail.com>
2025-03-23 17:02:34 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("scene") });
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
